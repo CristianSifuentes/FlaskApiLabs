c3ecc30dbf5e4d2c7772f1ede963ca97c4cb02d1
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-06-21 17:07:33 -0500

    20200621: Initialize the configuration for create diferents enviroment like development, production and others, Its was created the new class called config.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from app import create_app
+from config import config
 
-app = create_app()
+enviroment = config['development']
+app = create_app(enviroment)
 
 if __name__ == '__main__':
     app.run()
--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-def create_app():
+def create_app(enviroment):
+    app.confi.from_object(enviroment)
     return app
