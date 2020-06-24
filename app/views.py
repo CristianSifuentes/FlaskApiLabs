ed66326d854c95395d6d9576f17f0759150d5af3
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-06-24 18:55:35 -0500

    20200624: In this commit we start to use the configurtion for Blueprint where we can create routes for a rest api

---
+++ app/views.py
@@ -0,0 +1,32 @@
+from flask import jsonify
+from flask import Blueprint
+
+api_v1 = Blueprint('api',__name__, url_prefix='/api/v1')
+
+@api_v1.route('/tasks', methods=['GET'])
+  def get_tasks():
+      return jsonify({
+           'message': 'Hola, desde el endpoint listado de tareas!'
+      })
+
+
+@api_v1.route('/task', methods=['GET'])
+  def get_task():
+      pass
+    
+
+
+@api_v1.route('/tasks', methods=['POST'])
+  def create_task():
+      pass
+
+
+@api_v1.route('/tasks/<id>', methods=['PUT'])
+  def update_task():
+      pass
+    
+
+@api_v1.route('/tasks/<id>', methods=['DELETE'])
+  def delete_task():
+      pass
+    
