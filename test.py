c38a77845b40497cc098583bc215bf7b436c9efe
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-07-19 20:13:14 -0500

    20200719: We created a new test for create tasks

--- test.py
+++ test.py
@@ -40,6 +40,21 @@ class TestAPI(unittest.TestCase):
         response = self.client.get(path=new_path, content_type=self.content_type)
 
         self.assertEqual(response.status_code, 404)
+   
+    def test_create_task(self):
+        data = {
+            'title': 'title test', 
+            'description': 'description test', 
+            'deadline': '2019-12-12 12:00:00'
+        }
+        response = self.client.post(path=self.path, data=json.dumps(data), content_type=self.content_type)
+
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode('utf-8'))
+        task = data['data']
+        task_id = task['id']
+        
+        self.assertEqual(task_id, 3)
 
 if __name__ == '__main__':
     unittest.main()
